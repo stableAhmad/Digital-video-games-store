72dc70882a1e543cad022ff7da2cf849cd41fb9e
AhmadAbdelsalam <ahmedmohamed11782001@gmail.com>
2023-05-28 02:57:45 +0200

    item-model almost done

--- Backend Microservices/Items/api/models/item-model.js
+++ Backend Microservices/Items/api/models/item-model.js
@@ -1,11 +1,84 @@
 const mongoDB = require('../mongo.js')
 
 
+const projection = {"title":0, "price":0, "developer":0, "publisher":0,"sold_count":0, "release_date":0,
+                     "available":0, "off":0, "edition":0, "active":0, "description" :0 ,"requirements":0,
+                      "guide":0, "platform":0,"genre":0, "currency":0 , "language":0, "age":0 }
 
 
-async function read(DBClient){
+function resetProjection(){
+  for (let prop in projection) {
+      projection[prop] = 0;
+    }
+}
+async function findByID(DBClient, id){
+
+  try{
+
+      const query = { _id: ObjectId(id) }
+      const result = await DBClient.database.collection('item').findOne(query)
+      return result;
+
+  }catch(err){
+
+      console.log(err.message)
+      return false
+
+  }
 
 }
+
+async function findAllFull(DBClient){
+
+  try{
+
+      const result = await DBClient.database.collection('item').find().toArray()
+      return result;
+
+  }catch(err){
+
+      console.log(err.message)
+      return false
+
+  }
+
+}
+
+async function findAllPart(DBClient, attributes){
+
+  try{
+      resetProjection()
+      attributes.forEach(x => {
+        if(projection.hasOwnProperty(x)){
+            projection[x] = 1
+        }
+      })
+      const result = await DBClient.database.collection('item').find({}, projection).toArray()
+      return result;
+
+  }catch(err){
+
+      console.log(err.message)
+      return false
+
+  }
+
+}
+
+async function findBestSellers(DBClient){
+    try{
+        let res = await findAllFull(DBClient)
+        res.sort((a, b) => b.sold_count - a.sold_count)
+        return res
+    }catch(err){
+      console.log(err.message)
+    }
+}
+
+
+
+//missing website authorization
+
 
 async function update(DBClient){
 
