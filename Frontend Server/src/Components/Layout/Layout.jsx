7b786e8880860182057446ab160451c1cd61d4b9
Abdelrahman Elsayed <abdelrahman.elsayed0122@gmail.com>
2023-05-29 22:59:05 +0300

    Handle Register and Login

--- Frontend Server/src/Components/Layout/Layout.jsx
+++ Frontend Server/src/Components/Layout/Layout.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React,{useContext} from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import SideBar from '../SideBar/SideBar';
 import NavBar from '../NavBar/NavBar';
 import styles from '../Layout/Layout.module.css';
 import DarkMode from '../DarkMode/DarkMode';
+import { CartContext } from '../../Context/CartContext';
 
 export default function Layout() {
+  let {setUserData,userData} = useContext(CartContext)
+  let naigate = useNavigate();
+
+
   return (
     <div className={`${styles.layout} container-fluid m-0 py-5`}>
       <div className={`${styles.overlay}`}></div>
--- Frontend Server/src/Context/CartContext.js
+++ Frontend Server/src/Context/CartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState,useEffect } from "react";
 
 
 
@@ -7,6 +7,23 @@ export const CartContext = createContext(0)
 
 
 export default function CounterContextProvider(props) {
+
+    let [userData, setUserData] = useState(null);
+
+
+  
+  function saveUserData() {
+    setUserData(localStorage.getItem("userToken"));
+   console.log(userData);
+  }
+  useEffect(() => {
+    if (localStorage.getItem("userToken")) {
+      saveUserData();
+    }
+  }, [saveUserData]);
+
+
+
 
     const [cart, setCart] = useState(0)
     const [cartGames, setCartGames] = useState([])
@@ -24,7 +41,7 @@ export default function CounterContextProvider(props) {
         setCartItemsCount(--cartItemsCount)
     }
 
-    return <CartContext.Provider value={{ cart, createCart, cartGames, setCartGames, cartItemsCount, countIncrease, countDecrease }}>
+    return <CartContext.Provider value={{ cart, createCart, cartGames, setCartGames, cartItemsCount, countIncrease, countDecrease,userData,setUserData,saveUserData }}>
 
 
         {props.children}
